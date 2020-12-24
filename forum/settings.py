000074492c5c8358fb9bce2d23ed55b239399762
Sergey <biggreen.rm@gmail.com>
2020-12-24 16:00:07 +0300

    Register likedislike as application

--- forum/settings.py
+++ forum/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'topic.apps.TopicConfig',
     'comment.apps.CommentConfig',
+    'likedislike.apps.LikedislikeConfig',
 ]
 
 MIDDLEWARE = [
