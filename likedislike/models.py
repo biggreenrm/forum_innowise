efbaab98f7788a12b7f148375f94d038e7764a87
Sergey <biggreen.rm@gmail.com>
2020-12-24 16:07:06 +0300

    Create model for LikeDislike system

--- likedislike/models.py
+++ likedislike/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class LikeDislike(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+
+    VOTES = ((DISLIKE, "Dislike"), (LIKE, "Like"))
+
+    vote = models.SmallIntegerField(verbose_name=("Vote"), choices=VOTES)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
