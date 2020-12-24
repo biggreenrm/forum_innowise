6196d867fb003398e93bfddc2e6dfbbe7d99bb6e
Sergey <biggreen.rm@gmail.com>
2020-12-24 13:58:31 +0300

    Register Topic in admin panel

--- topic/admin.py
+++ topic/admin.py
@@ -1,3 +1,17 @@
+# First-party
+from .models import Topic
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "theme", "text", "created_date", "published_date")
+    list_filter = ("author", "title", "theme", "text", "created_date", "published_date")
+    search_fields = ("text",)
+    ordering = ("created_date", "author")
+
+
+# To-do:
+#   1. Сделать так, чтобы текст в админке сокращался
