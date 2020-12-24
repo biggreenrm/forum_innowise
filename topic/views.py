18327241063aa44fe58ed463392d9e46d1ae892b
Sergey <biggreen.rm@gmail.com>
2020-12-24 18:52:59 +0300

    Create basic view for list of all topics

--- topic/views.py
+++ topic/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# First-party
+from .models import Topic
+# Django
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
-# Create your views here.
+
+def topic_list(request):
+    # такой фильтр позволяет отсеить неопубликованные топики
+    topics = Topic.objects.all()
+    #import pdb; pdb.set_trace()
+    return render(request, "topic/topic_list.html", {"topics": topics})
