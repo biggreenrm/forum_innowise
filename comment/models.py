1a8758147b25742fb9fb7c1174d97be27a4e2cdc
Sergey <biggreen.rm@gmail.com>
2020-12-24 14:51:45 +0300

    Repair GenericForeignKey() for comment

--- comment/models.py
+++ comment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
@@ -7,8 +8,11 @@ class Comment(models.Model):
     """
     Модель для комментариев к темам на форуме, а также к самим комментариям.
     """
-    
+
     user = models.ForeignKey('auth.User', db_index=True, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    text = models.CharField(max_length=1000, default="")
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
