c60ed2b921c1be6844598e064e9da0c4cc226252
Sergey <biggreen.rm@gmail.com>
2020-12-24 15:58:22 +0300

    Change items listed in django admin panel for comment and topic models

--- comment/admin.py
+++ comment/admin.py
@@ -6,4 +6,4 @@ from django.contrib import admin
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user", "content_type", "object_id", "content_object", "created_date", "published_date")
+    list_display = ("id", "user", "text", "content_object", "created_date", "published_date")
