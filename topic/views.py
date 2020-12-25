5766b0d6ecac079db9c1afb64fd873e45fccac9b
Sergey <biggreen.rm@gmail.com>
2020-12-25 19:20:22 +0300

    will not use this

--- topic/views.py
+++ topic/views.py
@@ -4,10 +4,3 @@ from .models import Topic
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-def topic_list(request):
-    topics = Topic.objects.all() # не забыть отфильтровать топики по дате публикации (чтобы скрыть левые)
-    return render(request, "topic/topic_list.html", {"topics": topics})
-
-def topic_detail(request, id):
-    topic = get_object_or_404(Topic, id=id)
-    return render(request, "topic/topic_details.html", {"topic": topic})
