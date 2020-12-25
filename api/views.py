2071cfc3e56d93fc6ae62d259dffa4d446e5f0e5
Sergey <biggreen.rm@gmail.com>
2020-12-25 10:15:37 +0300

    Develop viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
+# First-party
+from topic.models import Topic
+from comment.models import Comment
+from likedislike.models import LikeDislike
+from .serializers import TopicSerializer, CommentSerializer, LikeDislikeSerializer
+# Django
 from django.shortcuts import render
+# Third-party
+from rest_frameworl import viewsets
+from django_filters import rest_framework as filters
 
-# Create your views here.
+
+class TopicViewSet(viewsets.ModelViewSet):
+    serializer_class = TopicSerializer
+    queryset = Topic.objects.all()
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ('author', 'title', 'theme', 'created_date')
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ('user', 'content_object', 'created_date')
+
+
+class LikeDislikeViewSet(viewsets.ModelViewSet):
+    serializer_class = LikeDislikeSerializer
+    queryset = LikeDislike.objects.all()
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ('user', 'vote', 'content_object')
