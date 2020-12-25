c57b66bbd016ed0e3b9667da65f77183605d556e
Sergey <biggreen.rm@gmail.com>
2020-12-25 19:21:13 +0300

    delete unnecesary fields from admin panel

--- comment/admin.py
+++ comment/admin.py
@@ -6,4 +6,4 @@ from django.contrib import admin
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "text", "content_object", "created_date", "published_date")
+    list_display = ("id", "user", "text", "content_object", "created_date", "published_status")
--- topic/admin.py
+++ topic/admin.py
@@ -7,8 +7,8 @@ from django.contrib import admin
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ("id", "author", "title", "theme", "text", "created_date", "published_date")
-    list_filter = ("author", "title", "theme", "text", "created_date", "published_date")
+    list_display = ("id", "author", "title", "theme", "text", "created_date", "published_status")
+    list_filter = ("author", "title", "theme", "text", "created_date", "published_status")
     search_fields = ("text",)
     ordering = ("created_date", "author")
 
