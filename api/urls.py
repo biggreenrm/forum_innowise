41cff23357afa0660edf4fc81314336d3fd6f035
Sergey <biggreen.rm@gmail.com>
2020-12-25 10:36:51 +0300

    create urls for api and connect it to the main urls.py

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+# First-party
+from .views import TopicViewSet, CommentViewSet, LikeDislikeViewSet
+# Django
+from django.urls import include, path
+# Third-party
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'topics', TopicViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'likesdislikes', LikeDislikeViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("topic.urls")),
-    path('api-auth/', include('rest_framework.urls')),
+    path('', include('api.urls')),
+    path('', include('topic.urls')),
+    
 ]
