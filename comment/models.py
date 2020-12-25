d098b3bb7a6d91eed2659d55bf0df325f9e8e730
Sergey <biggreen.rm@gmail.com>
2020-12-25 19:20:54 +0300

    add model methods to count likes

--- comment/models.py
+++ comment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from likedislike.models import LikeDislike
+
 
 
 class Comment(models.Model):
@@ -16,6 +18,7 @@ class Comment(models.Model):
     text = models.CharField(max_length=1000, default="")
     created_date = models.DateTimeField(default=timezone.now)
     published_status = models.BooleanField(default=False)
+    likes_dislikes = GenericRelation(LikeDislike)
 
     class Meta:
         ordering = ("created_date",)
@@ -23,3 +26,7 @@ class Comment(models.Model):
     def publish(self):
         if not self.published_status:
             self.published_status = True
+
+    @property
+    def total_likes_dislikes(self):
+        return self.likes_dislikes.count()
--- topic/models.py
+++ topic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from likedislike.models import LikeDislike
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Topic(models.Model):
@@ -14,6 +16,8 @@ class Topic(models.Model):
     text = models.TextField() # нужна ли максимальная длина? какие-нибудь другие параметры?
     created_date = models.DateTimeField(default=timezone.now)
     published_status = models.BooleanField(default=False)
+    likes_dislikes = GenericRelation(LikeDislike)
+
 
     class Meta:
         ordering = ("created_date",)
@@ -25,5 +29,9 @@ class Topic(models.Model):
     def __str__(self):
         return self.title
     
+    @property
+    def total_likes_dislikes(self):
+        return self.likes_dislikes.count()
+
     def get_absolute_url(self):
         return reverse("topic_detail", args=[self.id])
