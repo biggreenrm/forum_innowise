e006bfa47fbff4b8b2c58d6d1917f01758296064
Sergey <biggreen.rm@gmail.com>
2020-12-27 10:33:33 +0300

    delete not api urls and views

--- topic/urls.py
+++ topic/urls.py
@@ -2,9 +2,3 @@
 from . import views
 # Django
 from django.urls import path
-
-
-urlpatterns = [
-    path("", views.topic_list, name="topics_list"),
-    path("topic/<id>/", views.topic_detail, name="topic_detail"),
-]
